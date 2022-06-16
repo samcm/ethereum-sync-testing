721739dc97e6fd38cf4ccbea29bb871259373430
Sam Calder-Mason <sam@puritydev.io>
2022-06-16 11:16:45 +1000

    template github ci

--- deploy/ports.sh
+++ deploy/ports.sh
@@ -7,9 +7,9 @@ echo "{" > $file
 # Keep a track of the index
 index=30000
 for network in kiln ropsten; do
-  for test in basic complex1 complex2 complex3; do
+  for test in basic complex1 complex2 complex3 is-healthy starts-syncing; do
     for consensus in lighthouse prysm nimbus teku lodestar; do
-      for execution in geth besu nethermind; do 
+      for execution in geth besu nethermind erigon; do 
         echo \"$network-$test-$consensus-$execution\": { \"consensus\": $(($index+1)), \"execution\": $((index+2)) }, >> $file
 
         index=$((index + 2))
