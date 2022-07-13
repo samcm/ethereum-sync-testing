3d539e52b496a702a560e990c1c2ed9c8d9efc25
Sam Calder-Mason <sam@puritydev.io>
2022-07-13 14:30:06 +1000

    Add checkpoint syncing

--- deploy/ports.sh
+++ deploy/ports.sh
@@ -7,7 +7,7 @@ echo "{" > $file
 # Keep a track of the index
 index=30000
 for network in kiln ropsten; do
-  for test in to-head complex1 complex2 complex3 is-healthy starts-syncing; do
+  for test in to-head complex1 complex2 complex3 is-healthy starts-syncing checkpoint; do
     for consensus in lighthouse prysm nimbus teku lodestar; do
       for execution in geth besu nethermind erigon; do 
         echo \"$network-$test-$consensus-$execution\": { \"consensus\": $(($index+1)), \"execution\": $((index+2)) }, >> $file
--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -93,30 +93,30 @@ resource "digitalocean_kubernetes_node_pool" "ethereum-amd" {
   }
 }
 
-resource "digitalocean_kubernetes_node_pool" "ethereum-large" {
-  lifecycle {
-    ignore_changes = [
-      node_count,
-      nodes,
-    ]
-  }
+# resource "digitalocean_kubernetes_node_pool" "ethereum-large" {
+#   lifecycle {
+#     ignore_changes = [
+#       node_count,
+#       nodes,
+#     ]
+#   }
 
-  cluster_id = digitalocean_kubernetes_cluster.sync-testing.id
-  name       = "${local.cluster_name}-ethereum-large"
-  size       = "so1_5-4vcpu-32gb" # $250/month
-  auto_scale = true
-  max_nodes = 5
-  min_nodes = 1
-  node_count =  1
+#   cluster_id = digitalocean_kubernetes_cluster.sync-testing.id
+#   name       = "${local.cluster_name}-ethereum-large"
+#   size       = "so1_5-4vcpu-32gb" # $250/month
+#   auto_scale = true
+#   max_nodes = 5
+#   min_nodes = 1
+#   node_count =  1
 
-  tags       = concat(local.common_tags, ["ethereum"])
+#   tags       = concat(local.common_tags, ["ethereum"])
 
-  taint {
-    key    = "dedicated"
-    value  = "ethereum-large"
-    effect = "NoSchedule"
-  }
-}
+#   taint {
+#     key    = "dedicated"
+#     value  = "ethereum-large"
+#     effect = "NoSchedule"
+#   }
+# }
 
 resource "kubernetes_priority_class" "observability-critical" {
   metadata {
