0cffd182439bb578319c6afa11dad068dfbb4050
Sam Calder-Mason <sam@puritydev.io>
2022-08-24 13:52:59 +1000

    add mainnet tests

--- deploy/ports.sh
+++ deploy/ports.sh
@@ -6,8 +6,8 @@ echo "{" > $file
 
 # Keep a track of the index
 index=30000
-for network in kiln ropsten; do
-  for test in to-head complex1 complex2 complex3 is-healthy starts-syncing checkpoint; do
+for network in kiln ropsten mainnet; do
+  for test in to-head complex1 complex2 complex3 is-healthy starts-syncing checkpoint to-head-mainnet; do
     for consensus in lighthouse prysm nimbus teku lodestar; do
       for execution in geth besu nethermind erigon; do 
         echo \"$network-$test-$consensus-$execution\": { \"consensus\": $(($index+1)), \"execution\": $((index+2)) }, >> $file
